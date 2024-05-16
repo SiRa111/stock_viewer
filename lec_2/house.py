e01befea9e31c2d11f480f7b9ae38413a82efbdb
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-16 13:45:34 +0000

    Thu, May 16, 2024, 7:15 PM +05:30

--- lec_2/house.py
+++ lec_2/house.py
@@ -1,8 +1,22 @@
 name = input("What is your name ?")
 
+'''
 if name == "Harry" or name == "Hermione" or name == "Ron":
     print("Gryffindor")
 elif name == "Draco":
     print("Slytherin")
 else:
     print("Bruh. Who?")
+'''
+
+match name:
+    case "Harry" :
+        print("Gryffindor")
+    case "Hermione" :
+        print("Gryffindor")
+    case "Ron" :
+        print("Gryffindor")
+    case "Draco" :
+        print("Slytherin")
+    case _:
+        print("BRUH. Who ?")
