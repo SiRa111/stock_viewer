3370b71f1864bb53faacab8c30d78c0d5a73da12
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-16 12:58:03 +0000

    Thu, May 16, 2024, 6:28 PM +05:30

--- lec_2/grade.py
+++ lec_2/grade.py
@@ -3,11 +3,11 @@ score = int(input("Enter your score : "))
 def grade():
     if 90 <= score <= 100:
         return "Grade A "
-    elif score < 90:
+    elif score > 80:
         return "Grade B"
-    elif score < 80:
+    elif score > 70:
         return "Grade C"
-    elif score < 70:
+    elif score > 60:
         return "Grade D"
     else:
         return "Grade F"
