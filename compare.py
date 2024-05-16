625f476335e211e4267dc2cfeb1001d0efb4dba7
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-16 12:14:18 +0000

    Thu, May 16, 2024, 5:44 PM +05:30

---
+++ compare.py
@@ -0,0 +1,12 @@
+x = int(intput("Enter the value for x : "))
+y = int(intput("Enter the value for y : "))
+
+def main():
+    if x < y:
+        return "x is less than y."
+    elif x > y:
+        return "x is greater than y."
+    else:
+        return "x is equal to y."
+
+main()
