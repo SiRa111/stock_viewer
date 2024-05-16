ad1b9fac4cb7c188638f6a65c34fc3e39eec8aaf
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-16 12:41:42 +0000

    Thu, May 16, 2024, 6:11 PM +05:30

--- lec_2/grade.py
+++ lec_2/grade.py
@@ -5,4 +5,12 @@ def grade():
         return "Grade A "
     elif score < 90 and score >= 80:
         return "Grade B"
-    elif score < 70
+    elif score < 80 and score >= 70:
+        return "Grade C"
+    elif score < 70 and score >= 60:
+        return "Grade D"
+    else:
+        return "Grade F"
+
+a = grade()
+print(a)
