d75183d044982b8afb22113d58236996cf63a169
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-16 13:24:07 +0000

    Thu, May 16, 2024, 6:54 PM +05:30

--- lec_2/parity.py
+++ lec_2/parity.py
@@ -6,10 +6,8 @@ def main():
         print(f"{x} is odd")
 
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else :
-        return False
+    #return True if n % 2 == 0 else False
+    
 
 main()
 
