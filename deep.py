7d287e5745aac8e5c3111eb4c1993da0819ce49b
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-21 09:31:46 +0000

    Tue, May 21, 2024, 3:01 PM +05:30

--- deep.py
+++ deep.py
@@ -1,7 +1,7 @@
 ans = input("What is Answer to the Great Question of Life, the Universe, and Everything ? ")
-ans1 = ans.strip().lowercase()
-match ans:
-    case "42" | "forty-two" | "forty two" | "Forty Two" | "Forty-two" :
+ans1 = ans.strip().lower()
+match ans1:
+    case "42" | "forty-two" | "forty two"  :
         print("Yes")
     case _ :
         print("No")
