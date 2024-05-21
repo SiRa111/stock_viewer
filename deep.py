2bb7eee7a73cd57cbfe162d6d9b7ece3ddea9a40
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-21 09:26:28 +0000

    Tue, May 21, 2024, 2:56 PM +05:30

--- deep.py
+++ deep.py
@@ -1,4 +1,4 @@
-ans = input("What is Answer to the Great Question of Life, the Universe, and Everything ?")
+ans = input("What is Answer to the Great Question of Life, the Universe, and Everything ? ")
 
 match ans:
     case "42" | "forty-two" | "forty two" | "Forty Two" | "Forty-two" :
