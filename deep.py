a10e44cafefa232d9e53f826375b08b18b03654f
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-21 09:25:47 +0000

    Tue, May 21, 2024, 2:55 PM +05:30

---
+++ deep.py
@@ -0,0 +1,7 @@
+ans = input("What is Answer to the Great Question of Life, the Universe, and Everything ?")
+
+match ans:
+    case "42" | "forty-two" | "forty two" | "Forty Two" | "Forty-two" :
+        print("Yes")
+    case _ :
+        print("No")
