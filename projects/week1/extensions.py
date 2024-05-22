a4e1c0369e6b44b1beb3b257930a670a5f3a83fa
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-22 14:28:55 +0000

    Wed, May 22, 2024, 7:58 PM +05:30

--- projects/week1/extensions.py
+++ projects/week1/extensions.py
@@ -1,4 +1,17 @@
 f_name = input("Enter the file name with extension : ")
 
-match f_name:
-    case
+def main():
+    if f_name.endswith(".gif"):
+        return "image/gif"
+    elif f_name.endswith(".jpg"):
+        return "image/jpg"
+    elif f_name.endswith(".jpeg"):
+        return "image/jpg"
+    elif f_name.endswith(".png"):
+        return "image/png"
+    elif f_name.endswith("")
+    else:
+        return "application/octet-stream"
+
+a = main()
+print(a)
