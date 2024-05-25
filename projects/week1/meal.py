25afac09dfc5240ab6285e012e9baf0a230dcde0
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-25 12:46:06 +0000

    Sat, May 25, 2024, 6:16 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -4,12 +4,30 @@ def main():
 
 
 def convert(time):
+
     def c(t):
         t = t.rstrip(" ")
         t1, t2 = t.split(":")
-        
+        t1 = t1.float(0)
+        t2 = t2.float(2)
+        if t1 > 12:
+            t1 = t1 + 12
+        else:
+            pass
+        t2 = t2 * 0.016
+        tf = t1 + t2
+        if tf >= 7 and tf <= 8:
+            return "breakfast time"
+        elif tf >= 12 and tf <= 13:
+            return "lunch time"
+        elif tf >= 16 and tf <= 17:
+            return "dinner time"
     if time.endswith("a.m" or "p.m."):
         c(time)
+    else:
+        pass
+
+    
 
 if __name__ == "__main__":
     main()
