508dc3a71eec3f84f38e8669e4b05c2cbe3ac98d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-25 12:24:44 +0000

    Sat, May 25, 2024, 5:54 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -1,10 +1,11 @@
 def main():
-    ...
+    t = input("Enter the time in 24hr format or 12 hr format : ")
+    t1 = t.rstrip(" ")
+    convert(t1)
 
 
 def convert(time):
-    ...
-
+    
 
 if __name__ == "__main__":
     main()
