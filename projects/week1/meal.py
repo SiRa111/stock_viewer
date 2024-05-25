239f3359ddad5baa41d19bb348869db6c6af5ada
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-05-25 12:32:41 +0000

    Sat, May 25, 2024, 6:02 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -1,11 +1,13 @@
 def main():
     t = input("Enter the time in 24hr format or 12 hr format : ")
-    t1 = t.rstrip(" ")
     convert(t1)
 
 
 def convert(time):
     time1, time2 = time.strip(":")
+    if time1 >= 13 :
+
+
     time1 = time1.float()
 
 if __name__ == "__main__":
