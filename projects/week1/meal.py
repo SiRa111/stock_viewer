35f47de533f2ca87be466a10087082d4c79c9e0c
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-21 14:39:28 +0000

    Fri, Jun 21, 2024, 8:09 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -13,14 +13,14 @@ def convert(time):
     a = float(a)
     print(a)
 
-'''
+
     if 7.00 <= a <= 8.00:
         print("breakfast time")
     elif 12.00 <= a <= 13.00:
         print("lunch time")
     elif 18.00 <= a <= 19.00:
         print("dinner time")
-'''
+
 
 if __name__ == "__main__":
     main()
