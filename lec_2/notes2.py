fbf6c8ef0dc9f637cf32a9f311c2c38d27c1fb43
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-21 13:15:31 +0000

    Fri, Jun 21, 2024, 6:45 PM +05:30

--- lec_2/notes2.py
+++ lec_2/notes2.py
@@ -1,9 +1,24 @@
 '''
-CONDITIONALS allow the program to make a desicion 
+CONDITIONALS allow the program to make a desicion
 
 if x < y
 x < y is a boolean ie true or false value
 
+if x < y :          statement 1
+    print("yay")
+if x > y:           statement 2
+    print("welp")
+if x == y:          statement 3
+    print("um")
 
+each time  the if condition is run. ie if statement 1 is true the code runs then the second statement is also checked and the third too. similarly even if it were false all the three statements would be checked. for a program conssisting if billions of lines of code. this slows down the process so we make our code more efficient :
 
+if x > y:               s1
+    print("yay")
+elif x < y:             s2
+    print("welp")
+else :                  s3
+    print("x == y ")
+
+now if statement one is true the rest are ignored. the execution only proceeds if the statement is false.
 '''
