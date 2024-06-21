b3d7a8a22939fc393bb430d0dfb77757ed1477fa
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-21 13:53:40 +0000

    Fri, Jun 21, 2024, 7:23 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -1,10 +1,11 @@
 def main():
-    t = input("Enter the time : ")
+    t = input("Enter the time in 24hr format: ")
     convert(t)
 
 
 def convert(time):
-    
+    time
+
 
 
 if __name__ == "__main__":
