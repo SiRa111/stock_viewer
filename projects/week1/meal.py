cf926033e0b81188391c32f3c97553258a6b7533
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-21 14:42:37 +0000

    Fri, Jun 21, 2024, 8:12 PM +05:30

--- projects/week1/meal.py
+++ projects/week1/meal.py
@@ -11,15 +11,15 @@ def convert(time):
     a = a + b
     a = "{:.2f}".format(a) #gives two decimal points to a and turns it to a string as this is a f string
     a = float(a)
-    
 
-
-    if 7.00 <= a <= 8.00:
+if 7.00 <= a <= 8.00:
         print("breakfast time")
     elif 12.00 <= a <= 13.00:
         print("lunch time")
     elif 18.00 <= a <= 19.00:
         print("dinner time")
+
+
 
 
 if __name__ == "__main__":
