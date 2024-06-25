f889fe81c988eb8ffad4398aab0448bf56e91949
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-25 03:53:03 +0000

    Tue, Jun 25, 2024, 9:23 AM +05:30

--- lec_1/notes1.py
+++ lec_1/notes1.py
@@ -1,172 +1,57 @@
+'''
+CONDITIONALS allow the program to make a desicion
+
+if x < y
+x < y is a boolean ie true or false value
+
+if x < y :          statement 1
+    print("yay")
+if x > y:           statement 2
+    print("welp")
+if x == y:          statement 3
+    print("um")
+
+each time  the if condition is run. ie if statement 1 is true the code runs then the second statement is also checked and the third too. similarly even if it were false all the three statements would be checked. for a program conssisting if billions of lines of code. this slows down the process so we make our code more efficient :
+
+if x > y:               s1
+    print("yay")
+elif x < y:             s2
+    print("welp")
+else :                  s3
+    print("x == y ")
+
+now if statement one is true the rest are ignored. the execution only proceeds if the statement is false.
+
+0 OR
+you can enter more than one condtion to check
+if x < y or x > y:
+we are checking both. if either is true, the code will run
+
+more efficient way :
+if x != y:
+
+0 AND
+it checks more than two statements simultaneously. code is only executed if all the conditions are met
+if score >= 90 and score <= 100:
+
+more efficient :
+90 <= score <= 100:
+
+0 parity refers to whether a number is either even or odd.
+
+0 MATCH
+ name = input("What's your name? ")
+
+  match name:
+      case "Harry" | "Hermione" | "Ron":
+          print("Gryffindor")
+      case "Draco":
+          print("Slytherin")
+      case _:
+          print("Who?")
+
+STRINGS ARE IMMUTABLE
+ANY CHANGES MADE IN STRINGS ARE TRANSIENT
+
 
 '''
-TERMINAL COMMANDS
-_______________________________________________________________________________________________________________________________________________________
-ls - lists all the files
-
-cp - copies a file
-# cp org_file.py new_file.py
-cp indoor.py projects/indoor.py #making a new copy in another directory
-
-mv - moves a file or rename
-s it
-mv goodbye.py farewell.py //renaming it to another file
-mv farewell.py .. //moving the file out of the parent folder
-mv indoor.py week0/indoor.py #moving it to diff directory
-rm - removes a file
-rm farewell.py
-
-mkdir -  make a directory / folder
-
-cd - change directory
-cd folder/ {Both with or without the slashes is fine}
-
-rmdir - remove a directory
-
-clear - clears the terminal window #alt : l
-
-'''
-
-
-"""
-TO DEFINE A FXN
-
-def main():
-    print("Heyyyyyyyyyyy, WOrldddddddddddd")
-    print("ayassssssssssa. slayyyyyyyyyyyyyyyy")
-
-
-"""
-
-
-'''
-VSC - text editor / compiler
-top - text editor
-bottom - terminal
-
-To run code in terminal : python file_name.py
-
-Functions - actions that the language already knows how to perform
-eg - print, int, str
-
-Variable - container for a value
-
-num = 12
-the value on rhs is assigned to the variable at the lhs
-
-comments : #
-"""any text bw these is a multiline comment"""
-
-Pseudocode - explain each block of code for clarity
-
-
-str - a sequence of text/ characters in python
-arguments - factors that can influence the behaviour of the function
-parameters - actual value passed to the function
-
-default parameters for print() - sep=" ", end="\n" #brings the cursor to the new line after the string is printed
-
-
-#SEP-SEP-SEP-SEP-SEP-SEP-SEP
-print("a b c d e", sep="-") DOES NOT SEP AN ENTIRE STRING
-op: a b c d e
-
-print("a","b","c","d","e", sep="-") SEP THE VARIOUS STRS
-op: a-b-c-d-e
-
-escape sequence - \n for newline character
-\" - to display " inside a string
-
-FORMATTING STRINGS
-# Ask the user for their name
-name = input("What's your name? ")
-print(f"hello, {name}")
-
-String Methods
-
-name = input("What's your name? ")
-name = name.strip() #strips the spaces on the left and right side of the String
-
-0 F-STRINGS
-print(f"hello, {name}")
-
-0 str.format()
-print('We are the {} who say "{}!"'.format('knights', 'Ni'))
-print('{0} and {1}'.format('spam', 'eggs'))
-
-op: We are the knights who say Ni
-op: spam and eggs
-
-name.lstrip() : removes the spaces on the left side of the string
-name.rstrip() : removes the spaces on the right side of the strip
-
-0 name = name.capitalize()
-simran rawat '-->' Simran rawat
-
-0 name = name.title()
-simran rawat '->' Simran Rawat
-
-0 CONCISE CODE
-name = name.strip().title()
-
-0 MORE CONCISE
-name = input("What's your name? ").strip().title()
-
-0
-#the user enters some string
-#the string is saved in name variable
-#the strins is striped off spaces on both sides
-#the string is titled ie all the words of the string will be capitalized
-
-0 SPLITTING STRINGS
-a,b = name.split(" ")
-split : divides the code into multiple strings based on the argument provided and saves in diff variables
-
-0 typing - code file_name.py will automatically create that file  __term
-pressing tab after writing the code in interpreter. It autocompletes itself
-
-0 INT
-% - Modulus - gives remainder
-int() - converts the data from string to int datatype
-
-0 FLOAT
-float(bleh)
-returns value in floats
-
-0 ROUND
-round(number, ndigit=1/2/3)
-3 means it will show till 3 deicmal places
-# till what decimal place you want your number
-
-#in programming languages, [] anything between these is generally optional so it will have a default value if a value is not specified
-
-0 SYNTAX
-z=1000000
-print(z) --> 1000000
-print(f"{z:,}") --> 1,000,000
-print(f"{z:.2f}") --> specifying using fstring about how many decimal places to print. 2 in this case
-
-0 def fxn():
-to invent your own fucntion
-
-def(parameter = "default value"):
-this value will be printed when no other value is explicitly provided to the fxn
-
-def hola():
-
-0 SCOPE
-If a fxn is defined within a fxn it can only be called within that fxn as its scope is within the fxn  and hence using it outside will give undefined
-
-0 RETURN
-passes back the value
-ends the execution of the fxn call
-any code after return is not considered
- [RETURN](calculator.py)
-
- 
-
-[FLOW](hello.py)
-
-'''
-#EMOJIS ARE JUST CHARACTERS !!
-' You can save emojis as characters in python '
