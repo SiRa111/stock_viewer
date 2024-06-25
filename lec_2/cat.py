90a165a5ec65f530d68d8a7c959cccd2c6e8f095
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-25 04:10:32 +0000

    Tue, Jun 25, 2024, 9:40 AM +05:30

--- lec_2/cat.py
+++ lec_2/cat.py
@@ -10,4 +10,12 @@ i = 3                               1
 while i != 0:                       2
     print("meow", end = " ")        3
 '''
-#this sets up an infinite loop
+#this sets up an infinite loop as i will always be 3
+#thus statement 2 will always be true
+#flow : 2 > 3 > 2 > 3 > 2 > 3 ...
+#happens because we are not altering the value of i
+
+i = 3
+while i != 0:
+    print("meow", end = " ")
+    i = i - 1 #decerementing the value  of i 
