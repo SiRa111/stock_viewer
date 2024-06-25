87b4b4e8ac34c0c137d76ad64433ecc2734db079
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-25 04:06:55 +0000

    Tue, Jun 25, 2024, 9:36 AM +05:30

--- lec_2/cat.py
+++ lec_2/cat.py
@@ -5,6 +5,9 @@ print("meow")
 '''
 # to print meow 3 times but what if 1000 times
 
-i = 3
-while i != 0:
-    print("meow", end = "  ")
+'''
+i = 3                               1
+while i != 0:                       2
+    print("meow", end = " ")        3
+'''
+#this sets up an infinite loop
