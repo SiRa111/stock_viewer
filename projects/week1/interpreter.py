32b58a444df5e60124493f40aaa8cb4740ad77ea
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-27 11:27:08 +0000

    Thu, Jun 27, 2024, 4:57 PM +05:30

--- projects/week1/interpreter.py
+++ projects/week1/interpreter.py
@@ -1,1 +1,13 @@
-  
+def main():
+    x = int(input("Enter first number : "))
+    z = int(input("Enter second number : "))
+    y = input("Enter the operator : ")
+    result = operate(x,y,z)
+    result = {":.1f"}.format(result)
+    result = float(result)
+    print(result)
+
+def operate(a,b,c):
+    match b:
+        case b == '+':
+
