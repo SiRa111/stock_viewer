01a7fedf521d945f0787df1e32afc13692669c2d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-27 11:45:15 +0000

    Thu, Jun 27, 2024, 5:15 PM +05:30

--- projects/week1/interpreter.py
+++ projects/week1/interpreter.py
@@ -2,27 +2,27 @@ def main():
     expression = input("Enter the expression : ")
     x,y,z = expression.split(" ")
 
-    '''
+
     result = operate(x,y,z)
     result = "{:.1f}".format(result)
     result = float(result)
     print(result)
-    '''
+
 
 def operate(a,b,c):
     a = int(a)
-    b = int(c)
+    c = int(c)
+
     match b:
         case '+':
             final = a + c
-            print(final)
         case '-':
             final = a - c
         case '/':
             final = a / c
         case '*':
             final = a*c
-    #return final
+    return final
 
 main()
 
