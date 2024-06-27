70829d587bf8fd49ede611618f4050d5032d57ca
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-27 09:19:06 +0000

    Thu, Jun 27, 2024, 2:49 PM +05:30

--- lec_2/mario.py
+++ lec_2/mario.py
@@ -1,15 +1,17 @@
 def main():
     print_column(3)
+    print_row(4)
 
 def print_column(height):
     print("#\n" * height, end="")
-
-# /n prints them in new line
-# end reduces the last space of \n
+    # /n prints them in new line
+    # end reduces the last space of \n
     '''
     alternate to:
-
     for i in range(height):
         print("#")
+    '''
+def print_row(width):
+        print("?" * width, end="\n")
 
 main()
