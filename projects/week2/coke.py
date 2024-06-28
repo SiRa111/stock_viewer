3036ead71a684598c153ff50c3eff1178531cc63
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 14:50:16 +0000

    Fri, Jun 28, 2024, 8:20 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -3,9 +3,10 @@ total = 0
 if total <= 50 :
     while total != 50:
         cent = int(input("Insert coin : "))
-        due = 50 - total
+
         if cent == 25 or cent == 10 or cent == 5:
             total = cent + total
+            due = 50 - total
             print("Amount Due : ", due)
 
         else :
