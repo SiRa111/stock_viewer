cc30db65dde7b6966f02960ba3971ef097378e55
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 13:37:04 +0000

    Fri, Jun 28, 2024, 7:07 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -1,18 +1,14 @@
 def main():
     print("Amount Due : 50")
-    while 
-    cent = int(input("Insert Coin : "))
-    total= 0
-    if cent == 25 or cent == 10 or cent == 5 :
-        while total != 50:
-            total = total + cent
-            print("Amount Due : ",50 - total)
-            continue
-    else :
-        pass
-
-
-
-
+    while True:
+        cent = int(input("Insert Coin : "))
+        total= 0
+        if cent == 25 or cent == 10 or cent == 5 :
+            while total != 50:
+                total = total + cent
+                print("Amount Due : ",50 - total)
+                continue
+        else :
+            pass
 
 main()
