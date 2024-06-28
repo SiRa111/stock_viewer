3fbd962bd6ec860437c3db8c7ca4d384a44e25dc
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 13:11:12 +0000

    Fri, Jun 28, 2024, 6:41 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -1,5 +1,13 @@
 def main():
     cent = int(input("Amount Due : 50\nInsert Coin : "))
+    final = due(cent)
+
+def due(value):
+    total= 50
+    if value == 25 or value == 10 or value == 5 :
+        total = total - value
+        print("Amount Due : ",total)
+        
 
 
 main()
