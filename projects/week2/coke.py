3fe806753e4faaee92d9a4fe8010209da0356d13
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 14:11:19 +0000

    Fri, Jun 28, 2024, 7:41 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -1,11 +1,14 @@
 def main():
     print("Amount Due : 50")
     total = 50
-    while total >= 0:
+    while total != 0:
         cent = int(input("Insert Coin : "))
-        if cent == 25 or cent == 10 or cent == 5:
+        if cent == 25 or cent == 10 or cent == 5 and total > cent:
             total = total - cent
             print("Amount Due : ",  total)
+        elif total < cent :
+            change = cent - total
+            print("Change owed : ", change)
         else :
             print("Amount Due : ",  total)
             pass
