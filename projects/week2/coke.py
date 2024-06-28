efaf4ae9d31fbe2bc07b3f15d04628f042c47d51
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 13:21:19 +0000

    Fri, Jun 28, 2024, 6:51 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -4,12 +4,11 @@ def main():
     due(cent)
 
 def due(value):
-    total= 50
-
+    total= 0
     if value == 25 or value == 10 or value == 5 :
         while total != 0:
-            total = total - value
-            print("Amount Due : ",total)
+            total = total + value
+            print("Amount Due : ",50 - total)
             continue
     elif
 
