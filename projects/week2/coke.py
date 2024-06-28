eb842b13b26973287b13f9ebd295cefdda14e34e
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-28 13:43:44 +0000

    Fri, Jun 28, 2024, 7:13 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -1,10 +1,11 @@
 def main():
     print("Amount Due : 50")
-    total = 0
+    total = int(0)
     while total != 50:
         cent = int(input("Insert Coin : "))
         total = total + cent
-        
+        print("Amount Due : ", 50 - total)
+
 
 
 main()
