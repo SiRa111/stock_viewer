8b515b9972e177c2062ed4c107dd032986d70e01
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-29 08:48:08 +0000

    Sat, Jun 29, 2024, 2:18 PM +05:30

--- projects/week2/twttr.py
+++ projects/week2/twttr.py
@@ -6,7 +6,7 @@ def process(string):
     l = []
     for i in range(len(string)):
 
-        if i == 'a'|'A'|'e'|'E'|'i'|'I'|'o'|'O'|'u'|'U':
+        if i == 'a' or 'A' or 'e' or 'E' or 'i' or 'I' or 'o' or 'O' or 'u' or 'U':
             pass
         l[i] = string[i]
 
