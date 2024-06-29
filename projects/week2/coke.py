4f41e029ac8b0b1e335eac7e1f4fb8661c3c99b6
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-29 07:02:18 +0000

    Sat, Jun 29, 2024, 12:32 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -1,5 +1,15 @@
 def main():
     print("Amount Due : 50")
+    total = 0
+    while total != 50:
+        cent = int(input("Insert Coin : "))
+        total = total + cent
+        if total <= 50 :
+            prod(cent)
+        elif total > 50:
+            change(cent)
 
+def prod(a):
+    
 
 main()
