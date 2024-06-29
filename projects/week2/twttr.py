1a6a71ea581f606c9e79a2ed3f7f589d2e92bcaf
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-29 08:47:25 +0000

    Sat, Jun 29, 2024, 2:17 PM +05:30

--- projects/week2/twttr.py
+++ projects/week2/twttr.py
@@ -3,5 +3,13 @@ def main():
     process(str)
 
 def process(string):
-    
+    l = []
+    for i in range(len(string)):
+
+        if i == 'a'|'A'|'e'|'E'|'i'|'I'|'o'|'O'|'u'|'U':
+            pass
+        l[i] = string[i]
+
+    for i in l:
+        print(i, sep="", end="")
 main()
