24a3b266927f46e375f8e3eced8ca106f64e0810
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-29 07:59:41 +0000

    Sat, Jun 29, 2024, 1:29 PM +05:30

--- projects/week2/coke.py
+++ projects/week2/coke.py
@@ -7,10 +7,11 @@ def main():
         if cent == 5 or cent == 10 or cent == 25:
             total = total + cent
             if total < 50 :
-                print("Amount Due: ", 50 - total)
                 if 50 - total == 0:
                     print("Change Owed: ", 0)
                     break
+                print("Amount Due: ", 50 - total)
+
             elif total > 50:
                 print("Change Owed: ", total - 50 )
                 break
