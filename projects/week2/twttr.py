1c9ba3443ea86348217cf02ac1102c95696e325d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-29 09:27:35 +0000

    Sat, Jun 29, 2024, 2:57 PM +05:30

--- projects/week2/twttr.py
+++ projects/week2/twttr.py
@@ -12,6 +12,10 @@ def process(string):
             l.remove(i)
 
     for i in l:
+        if i == 'a' or  i == 'e' or i == "o" or i == "u" or i == "i" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U":
+            l.remove(i)
+
+    for i in l:
         print(i, end='')
 
 
