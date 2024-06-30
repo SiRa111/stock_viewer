3315fdf628a9d39594c6aeb501ea475f56a366e1
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-30 09:03:23 +0000

    Sun, Jun 30, 2024, 2:33 PM +05:30

--- projects/week2/plates.py
+++ projects/week2/plates.py
@@ -7,13 +7,12 @@ def main():
 
 
 def is_valid(s):
-    c1(s)
 
-
-def c1(s):
     l = []
     for i in range(len(s)):
         l.append(s[i])
+
+    #checks if first two letters are alphabets
     if len(l) >= 2:
         for i in range(2):
             if l[i].isalpha():
@@ -22,23 +21,23 @@ def c1(s):
                 return False
     else:
         return False
-    return True
+
+    #maximum six and minimum two characters
+    if 2 <= len(l) <= 6:
+        print(len(l))
+        pass
+    else:
+        return False
 
 
 
 
-def c2(string):
-    l = []
-    for i in range(len(string)):
-        l.append(string[i])
-    print(l)
-    d = 0
-    for i in range(len(l)):
-        print(i)
-        if 2 <= d <= 6:
-            return True
-        else:
-            return False
+
+
+
+
+
+
 
 
 main()
