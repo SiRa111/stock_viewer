f6c97d340b2c8d9620422ed5629c9534459eb5c7
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-06-30 10:37:05 +0000

    Sun, Jun 30, 2024, 4:07 PM +05:30

--- projects/week2/plates.py
+++ projects/week2/plates.py
@@ -35,7 +35,8 @@ def is_valid(s):
             continue
         else:
             return False
-    for i in range(len(l), -1, -1)
+    for i in range(len(l), 0, -1):
+        
 
     #the first digit cannot be zero
     for i in range(len(e)):
