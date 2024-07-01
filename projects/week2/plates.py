bc18262b2406674ffb34a724f93cf652d48a8eac
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-01 05:54:03 +0000

    Mon, Jul 1, 2024, 11:24 AM +05:30

--- projects/week2/plates.py
+++ projects/week2/plates.py
@@ -12,22 +12,20 @@ def is_valid(s):
     for i in range(len(s)):
         l.append(s[i])
 
-    #checks if first two letters are alphabets
-    if len(l) >= 2:
-        for i in range(2):
-            if l[i].isalpha():
-                continue
-            else:
-                return False
-    else:
-        return False
-
     #maximum six and minimum two characters
     if 2 <= len(l) <= 6:
         print(len(l))
         pass
     else:
         return False
+
+    #checks if first two letters are alphabets.
+    if len(l) >= 2:
+        for i in range(2):
+            if l[i].isalpha():
+                continue
+            else:
+                return False
 
     #makes sure only digits and letters are entered
     for i in range(len(l)):
