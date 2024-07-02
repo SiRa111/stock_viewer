7bfc93d77727fb6fc446cc8ac6e608b6aa3408ab
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-02 13:02:02 +0000

    Tue, Jul 2, 2024, 6:32 PM +05:30

--- projects/week2/plates.py
+++ projects/week2/plates.py
@@ -12,13 +12,14 @@ def is_valid(s):
     for i in range(len(s)):
         l.append(s[i])
 
+    #maximum six and minimum two characters
     if 2 <= len(l) <= 6:
         print(len(l))
         pass
     else:
         return False
 
-
+    #checks if first two letters are alphabets.
     if len(l) >= 2:
         for i in range(2):
             if l[i].isalpha():
@@ -26,14 +27,14 @@ def is_valid(s):
             else:
                 return False
 
-
+    #makes sure only digits and letters are entered
     for i in range(len(l)):
         if l[i].isalnum():
             continue
         else:
             return False
 
-
+    #the first digit cannot be zero
     e=[]
     for i in range(len(l)):
         if l[i].isnumeric():
@@ -41,7 +42,7 @@ def is_valid(s):
             if e[0] == '0':
                 return False
 
-   
+    #numbers are only at the end of the plate
     digits=[]
     num_index = int()
     for i in range(len(l)):
