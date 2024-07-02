8373d4d5335f74d477ecca90c92dd4417a28195d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-02 14:20:25 +0000

    Tue, Jul 2, 2024, 7:50 PM +05:30

--- projects/week2/nutrition.py
+++ projects/week2/nutrition.py
@@ -4,21 +4,21 @@ def main():
 
 dict = {"apple":130,
         "avocado":50,
-        "banana":110
-        "cantalopue":50
-        "grapefruit":60
-        "grapes":90
-        "honeydew melon":50
+        "banana":110,
+        "cantalopue":50,
+        "grapefruit":60,
+        "grapes":90,
+        "honeydew melon":50,
         "kiwifruit":90,
-        "lemon":15
-        "lime":20
-        "nectarine":60
-        "orange":80
-        "peach":60
-        "pineapple":50
-        "plums":70
-        "strawberries":50
-        "sweet cherries":100
-        "tangerine":50
+        "lemon":15,
+        "lime":20,
+        "nectarine":60,
+        "orange":80,
+        "peach":60,
+        "pineapple":50,
+        "plums":70,
+        "strawberries":50,
+        "sweet cherries":100,
+        "tangerine":50,
         "watermelon":80}
 main()
