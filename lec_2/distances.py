7c23272164e2b46e4f5809e3f7049ec4024ff9ef
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-02 15:34:50 +0000

    Tue, Jul 2, 2024, 9:04 PM +05:30

--- lec_2/distances.py
+++ lec_2/distances.py
@@ -10,4 +10,12 @@ def main():
     for name in distances.keys():
         print(f"{name} is the key -->{distances[name]} is the value")
         #this loop iterates over the KEYS of the dictionary
+    print(f"{'_'}"*30)
+    print("\n")
+
+    for distance in distances.values():
+        print(f"{distance} AU id {convert(distance)} in meteres")
+
+def convert(au):
+    return au * 149597870700
 main()
