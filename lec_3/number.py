3cbdcfad6a9307da9627eb9ff50227792071842d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-03 09:20:45 +0000

    Wed, Jul 3, 2024, 2:50 PM +05:30

--- lec_3/number.py
+++ lec_3/number.py
@@ -14,13 +14,15 @@ ERROR RECEIVED : NameError
 why: in line two int() fxn wants only int value but we enter a string. so a ValueError occurs first. due to this error on RHS in line2, no value was assigned to the variable called x.
 Hence, x was never defined and therefore when we try to print it in line6. We get NameError.
 '''
-
-try:
-    x = int(input("Enter value for x : ")) #try this statement
-except ValueError:
-    print("Your entered value is not an integer.") #if the specified error is raised. execute this statement
-else:
-    print(f"x is {x}") #if no error is raised. execute the else block
+while True: #infinite loop that runs until an integer is entered
+    try:
+        x = int(input("Enter value for x : ")) #try this statement
+    except ValueError:
+        print("Your entered value is not an integer.") #if the specified error is raised. execute this statement
+    else:
+        print(f"x is {x}")
+        #if no error is raised. execute the else block
+        break
 '''
 flow of code :
 input: cat --> 1 2 3 4. as cat would raise a valueError
