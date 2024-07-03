e8ed08fbc480a9a5dd2ca8b035e07a8bf006c177
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-03 08:46:45 +0000

    Wed, Jul 3, 2024, 2:16 PM +05:30

--- lec_3/notes3.py
+++ lec_3/notes3.py
@@ -1,2 +1,7 @@
 )SyntaxError : error in the written code
 eg: incomplete string
+
+)ValueError : another value type was entered than the one anticipated
+eg: int(input("enter Input here: "))
+input: "cat" , 78.90
+both these are not int so we get ValueError
