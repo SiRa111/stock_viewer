5da2dbd460b32b466404811922b2dbad4bd558e8
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-03 09:06:26 +0000

    Wed, Jul 3, 2024, 2:36 PM +05:30

--- lec_3/number.py
+++ lec_3/number.py
@@ -1,5 +1,15 @@
 try:
-    x = int(input("Enter value for x : "))
-    print(f"x is {x}")
+    x = int(input("Enter value for x : "))          #2
 except ValueError:
-    print("Your entered value is not an integer.")
+    print("Your entered value is not an integer.")  #4
+
+print(f"x is {x}")      #6
+
+'''
+input: cat
+ERROR RECEIVED : NameError
+'x' is not defined
+
+why: in line two int() fxn wants only int value but we enter a string. so a ValueError occurs first. due to this error on RHS in line2, no value was assigned to the variable called x.
+Hence, x was never defined and therefore when we try to print it in line6. We get name error.
+'''
