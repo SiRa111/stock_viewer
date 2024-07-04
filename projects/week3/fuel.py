1babf134a3342873cb444da455166121e9458f28
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-04 09:47:41 +0000

    Thu, Jul 4, 2024, 3:17 PM +05:30

--- projects/week3/fuel.py
+++ projects/week3/fuel.py
@@ -1,31 +1,7 @@
 def main():
     fuel = input("Fraction: ")
     n,d = fuel.split('/')
-    final = run(n,d)
 
-    if final == 1:
-        print("F")
-    elif final == 0:
-        print("E")
-    elif final > 1:
-        main()
-        return
-    else:
-        print(final,"%")
-
-def run(a,b):
-    try:
-        if a.isdigit() and b.isdigit():
-            a = int(a)
-            b = int(b)
-            return int(a/b)
-        else:
-            pass
-    except ZeroDivisionError:
-        main()
-        return
-    except ValueError:
-        main()
-        return
+    if n.isdigit() and
 
 main()
