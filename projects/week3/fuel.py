3be9dae4cf497ced68380050ac460d0afffab6d8
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-04 09:31:25 +0000

    Thu, Jul 4, 2024, 3:01 PM +05:30

--- projects/week3/fuel.py
+++ projects/week3/fuel.py
@@ -1,13 +1,16 @@
 def main():
     fuel = input("Fraction: ")
-    n,d = fuel.split('/')
     n = int()
     d = int()
+    n,d = fuel.split('/')
     final = run(n,d)
     if final == 1:
         print("F")
     elif final == 0:
         print("E")
+    elif final > 1:
+        main()
+        return
     else:
         print(final,"%")
 
@@ -20,8 +23,5 @@ def run(a,b):
     except ValueError:
         main()
         return
-    except a>b :
-        main()
-        return
-    
+
 main()
