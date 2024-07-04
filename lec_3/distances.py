0d9c6ead086c80ccc509bd8878f1bccbf703819c
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-04 08:35:39 +0000

    Thu, Jul 4, 2024, 2:05 PM +05:30

---
+++ lec_3/distances.py
@@ -0,0 +1,15 @@
+distances = {
+    "Voyager 1": "163",
+    "Voyager 2": "136",
+    "pioneer 10": "80 AU",
+    "New Horizons": "58",
+    "Pioneer 11": "44 AU"
+}
+
+def main():
+    spacecraft = input("Enter a spacecraft; ")
+    m = convert(distances[spacecraft])
+    print(f"{m} m away")
+
+def convert(au):
+    return au * 149597870700
