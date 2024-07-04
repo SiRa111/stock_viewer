4342a30834a24bd60f22d3d5855125ac035109ef
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-04 09:26:22 +0000

    Thu, Jul 4, 2024, 2:56 PM +05:30

--- projects/week3/fuel.py
+++ projects/week3/fuel.py
@@ -1,9 +1,26 @@
 def main():
     fuel = input("Fraction: ")
     n,d = fuel.split('/')
+    n = int()
+    d = int()
     final = run(n,d)
-    print(final)
+    if final == 1:
+        print("F")
+    elif final == 0:
+        print("E")
+    else:
+        print(final,"%")
 
 def run(a,b):
-    
+    try:
+        return int(a/b)
+    except ZeroDivisonError:
+        main()
+        return
+    except ValueError:
+        main()
+        return
+    except a>b :
+        main()
+        return
 main()
