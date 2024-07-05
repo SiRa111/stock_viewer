e49e45183461b252a89b26e09e8e0b8984b21a2e
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-05 10:20:48 +0000

    Fri, Jul 5, 2024, 3:50 PM +05:30

--- projects/week3/taqueria.py
+++ projects/week3/taqueria.py
@@ -1,22 +1,6 @@
 def main():
-  try:
-    menu = {
-            "Baja Taco": 4.25,
-            "Burrito": 7.50,
-            "Bowl": 8.50,
-            "Nachos": 11.00,
-            "Quesadilla": 8.50,
-            "Super Burrito": 8.50,
-            "Super Quesadilla": 9.50,
-            "Taco": 3.00,
-            "Tortilla Salad": 8.00
-            }
-    while True:
-      item = input("Item: ")
-      if item in menu:
-        total = total + menu[item]
-        print(total)
-    
+  
+
 
 
 main()
