028b8e0f8ccfd77e1bf20e5bc85badd439f5119b
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-06 07:06:11 +0000

    Sat, Jul 6, 2024, 12:36 PM +05:30

--- projects/week3/grocery.py
+++ projects/week3/grocery.py
@@ -3,11 +3,12 @@ def  main():
   while True:
     try:
       item = input()
-      list.update({item:1})
+
       if item in list:
         n = list[item]
         list.update({item:n+1})
-
+      list.update({item:1})
+      
     except EOFError:
       break
   print(list)
