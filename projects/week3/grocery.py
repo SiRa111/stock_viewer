ea5c30d3b64c6b36cb06472ab54c86c7423da712
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-06 06:53:41 +0000

    Sat, Jul 6, 2024, 12:23 PM +05:30

--- projects/week3/grocery.py
+++ projects/week3/grocery.py
@@ -4,10 +4,19 @@ def  main():
     try:
       item = input()
       for _ in list():
+        n = 0
         if item in list():
+          n += 1
           list.update(item:n)
         else:
-          list.update
+          list.update(item:1)
+
+    except EOFError:
+      break
+  list = list.sort()
+  for i in list:
+    print(list[i], i)
+
 
 
 main()
