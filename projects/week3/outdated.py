87ee0f3caedb968f7d4d14461a499e19b9bc8b50
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-06 08:01:24 +0000

    Sat, Jul 6, 2024, 1:31 PM +05:30

--- projects/week3/outdated.py
+++ projects/week3/outdated.py
@@ -1,6 +1,20 @@
 def main():
   date = input()
   month,day,year = date.split("/"," ",)
+  valid_month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
   
 
 main()
