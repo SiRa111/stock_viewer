d026d39746f80e8f3a79b6ebbd27356c02f25385
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-07 09:22:43 +0000

    Sun, Jul 7, 2024, 2:52 PM +05:30

--- lec_4/generate.py
+++ lec_4/generate.py
@@ -1,4 +1,14 @@
+'''
+MEthOD 1
 import random
+#youre importing all the functions from the random library
 
 coin = random.choice(["heads", "tails"])
+print(coin)
+'''
+
+#METHOD 2
+from random import choice
+
+coin = choice(["heads","tails"])
 print(coin)
