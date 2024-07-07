7ab28f2bc71a32acf0ccf0320335968c05d7ab16
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-07 09:26:57 +0000

    Sun, Jul 7, 2024, 2:56 PM +05:30

--- lec_4/notes4.py
+++ lec_4/notes4.py
@@ -2,7 +2,7 @@
 '''
 
 # 0 random
-random.choice(seq)
+___random.choice(seq)
 the seq will be a sequence.
 eg: list, string
 the choice will be made from the elements of the sequence
@@ -10,3 +10,7 @@ the choice will be made from the elements of the sequence
 ** if youre using from
 syntax:
 coin = choice(['a','b'])
+
+___random.randint(a,b)
+it picks a random int bw a and b.
+a and b are inclusive
