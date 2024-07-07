8d2f4b03699278bf1dd814a6741e112a16011840
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-07 09:24:11 +0000

    Sun, Jul 7, 2024, 2:54 PM +05:30

--- lec_4/notes4.py
+++ lec_4/notes4.py
@@ -6,3 +6,7 @@ random.choice(seq)
 the seq will be a sequence.
 eg: list, string
 the choice will be made from the elements of the sequence
+
+** if youre using from
+syntax:
+coin = choice(['a','b'])
