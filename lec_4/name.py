60ab4cea7a1a6a8efe1aeca1863fb2acc6ed0530
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-08 07:03:51 +0000

    Mon, Jul 8, 2024, 12:33 PM +05:30

--- lec_4/name.py
+++ lec_4/name.py
@@ -1,5 +1,11 @@
 import sys
 
+'''
 final = "hello,my name is " + sys.argv[1]
 print(final)
 #if you dont input any value during the command you get IndexError
+'''
+try:
+  print("hello, my name is", sys.argv[1])
+except:
+  print("Too few arguments")
