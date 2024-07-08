038360b2c8e0fc50b2c2096c21e8f9717e3d7cb1
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-08 07:52:52 +0000

    Mon, Jul 8, 2024, 1:22 PM +05:30

--- lec_4/notes4.py
+++ lec_4/notes4.py
@@ -36,6 +36,10 @@ Simran is stored in sys.argv[1]
 __sys.exit - exits the command line
 
 
-#i
+#iterating over the list here
 for arg in sys.argv[1:]: #slicing the list here. printing from index 1
   print("My name is",arg)
+
+__
+[1:] from index one till end. 1 included
+[1:-1] from index 1 till the last index. LAST EXCLUDED
