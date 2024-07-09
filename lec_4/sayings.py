111ec7914d85b6ffcd55a54ba997dd9dca99aeaf
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-09 06:26:57 +0000

    Tue, Jul 9, 2024, 11:56 AM +05:30

--- lec_4/sayings.py
+++ lec_4/sayings.py
@@ -16,3 +16,7 @@ if __name__ == "__main__":
 #this part will run only when we run the program directly during checking
 #it will not run when you import it as a module in other files as
 __name__ == "the_filename" and hence it will not be "main"
+
+
+pep8 : python enhancement proposal
+tries to standardise the code that we write
