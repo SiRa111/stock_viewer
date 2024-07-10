28d7d67403cc00211334150952373adf79d36ac3
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-10 06:41:24 +0000

    Wed, Jul 10, 2024, 12:11 PM +05:30

--- projects/week4/emojize.py
+++ projects/week4/emojize.py
@@ -8,9 +8,9 @@ def emo(string):
   a,b = string.split(":",1)
   b = ":" + b
   if '_' in b:
-    print(emoji.emojize(f"a, {b}"))
+    print(emoji.emojize(f"{a} {b}"))
   elif '_' not in b:
-    print(emoji.emojize(f"a, {b}", language= 'alias'))
+    print(emoji.emojize(f"{a} {b}", language= 'alias'))
 
 
 main()
