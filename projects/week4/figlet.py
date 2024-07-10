e3bb02d71532d404b9615dcaa0ab8fc4fbc68f1e
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-10 08:25:57 +0000

    Wed, Jul 10, 2024, 1:55 PM +05:30

--- projects/week4/figlet.py
+++ projects/week4/figlet.py
@@ -17,9 +17,18 @@ def convert(string):
         a = figlet.renderText(string)
         print(a)
     elif len(sys.argv) == 4:
-        
-
-
+        valid_font = figlet.getFonts()
+        if sys.argv[2] == "-f" or "-font":
+            if sys.argv[3] in valid_font:
+                figlet.setFont(font=sys.argv[3])
+                a = figlet.renderText(string)
+                print(a)
+            else:
+                print("Invalid usage")
+                sys.exit
+        else:
+            print("Invalid usage")
+            sys.exit
 
 
 if __name__ == "__main__":
