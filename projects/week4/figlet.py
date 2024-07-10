cc277c1c700f01631135fd435b5f05ed933519fc
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-10 07:53:11 +0000

    Wed, Jul 10, 2024, 1:23 PM +05:30

--- projects/week4/figlet.py
+++ projects/week4/figlet.py
@@ -1,8 +1,16 @@
 import figlet
 import sys
+import random
 
 def main():
   idk = input("Input: ")
+  convert(idk)
+
+def convert(string):
+  if len(sys.argv) == 2:
+    a = random.choice(figlet)
+    return figlet.a
+
 
 
 if __name__ == "__main__":
