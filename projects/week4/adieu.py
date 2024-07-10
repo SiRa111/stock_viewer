3144e3f3e40cf0f9141e406069bde4f98c70089a
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-10 10:25:50 +0000

    Wed, Jul 10, 2024, 3:55 PM +05:30

--- projects/week4/adieu.py
+++ projects/week4/adieu.py
@@ -1,12 +1,18 @@
 def main():
   l = ["Adieu, ", "adieu, ", "to "]
-  while True:
-    try:
-      name = input("Name: ")
-      l.append(name)
-      if len()
-    except EOFError:
-      return l
+  try:
+    while True:
+      if len(l) < 4:
+        name = input("Name: ")
+        l.append(name)
+      elif len(l) > 4:
+        name = input("Name: ")
+        l.append(name)
+        l.append([-2],"and")
+
+
+  except EOFError:
+    return l
 
 a = main()
 for _ in a:
