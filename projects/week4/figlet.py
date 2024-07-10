ea8ab21e88ab5bdea73b891584b18f36af4b1fe7
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-10 08:18:47 +0000

    Wed, Jul 10, 2024, 1:48 PM +05:30

--- projects/week4/figlet.py
+++ projects/week4/figlet.py
@@ -4,16 +4,20 @@ import random
 
 figlet = Figlet()
 
+
 def main():
     idk = input("Input: ")
     convert(idk)
 
 
 def convert(string):
-    f = random.choice(figlet.getFonts())
-    figlet.setFont(font=f)
-    a = figlet.renderText(string)
-    print(a)
+    if len(sys.argv) == 2:
+        f = random.choice(figlet.getFonts())
+        figlet.setFont(font=f)
+        a = figlet.renderText(string)
+        print(a)
+    elif len(sys.argv) == 4:
+        
 
 
 
