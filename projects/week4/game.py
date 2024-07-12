6bf265c321165c677d9c10583b80089d0b8efdca
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-12 06:53:22 +0000

    Fri, Jul 12, 2024, 12:23 PM +05:30

--- projects/week4/game.py
+++ projects/week4/game.py
@@ -1,6 +1,26 @@
 import random
 
 def main():
-  
+  while True:
+    num = int(input("Level: "))
+    if num > 0:
+      game(num)
+    else:
+      pass
+
+def game(n):
+  ans = random.randint(n)
+  while True:
+    guess = int(input("Guess: "))
+    if guess < n:
+      print("Too small!")
+    elif guess > n:
+      print("Too large!")
+    elif guess == n:
+      print("Just Right!")
+      break
+    else:
+      pass
+
 
 main()
