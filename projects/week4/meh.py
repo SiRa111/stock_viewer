d156dc08d7221c697a0a4ed6f4f205b83dc3a99f
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-12 13:36:46 +0000

    Fri, Jul 12, 2024, 7:06 PM +05:30

--- projects/week4/meh.py
+++ projects/week4/meh.py
@@ -1,7 +1,7 @@
 match [1, 2]:
-  case (a, b, c):
+  case a, b, c:
     print(a, b, c) # wont run
-  case (x):
+  case x:
     print(x) # will run
-  case (a, b):
+  case a, b:
     print(a, b) # should run but maybe x will eat it first
