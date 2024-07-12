a98ace22f36100ac75a06d471f3d8672ded6b7a1
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-12 07:11:53 +0000

    Fri, Jul 12, 2024, 12:41 PM +05:30

--- projects/week4/game.py
+++ projects/week4/game.py
@@ -1,34 +1,37 @@
 import random
 
+
 def main():
-  while True:
-    try:
-      num = int(input("Level: "))
-      if num > 0:
-        game(num)
-        break
-      else:
-        pass
-    except ValueError:
-      pass
-  return
+    while True:
+        try:
+            num = int(input("Level: "))
+            if num > 0:
+                game(num)
+                break
+            else:
+                pass
+        except ValueError:
+            pass
+    return
+
+
 def game(n):
-  ans = random.randint(1,n)
-  print(ans)
-  while True:
-    try:
-      guess = int(input("Guess: "))
-      if guess < ans and guess > 0:
-        print("Too small!")
-      elif guess > ans and guess > 0:
-        print("Too large!")
-      elif guess == ans:
-        print("Just Right!")
-        return
-      else:
-        pass
-    except ValueError:
-      pass
+    ans = random.randint(1, n)
+    print(ans)
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            if guess < ans and guess > 0:
+                print("Too small!")
+            elif guess > ans and guess > 0:
+                print("Too large!")
+            elif guess == ans:
+                print("Just Right!")
+                return
+            else:
+                pass
+        except ValueError:
+            pass
 
 
 main()
