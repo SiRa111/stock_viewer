e8d4212c825b5f9b8a2095e2f26c239d08734012
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-12 09:53:36 +0000

    Fri, Jul 12, 2024, 3:23 PM +05:30

--- projects/week4/professor.py
+++ projects/week4/professor.py
@@ -2,7 +2,8 @@ import random
 
 
 def main():
-    get_level()
+    final = get_level()
+    print(final)
 
 
 def get_level():
@@ -10,8 +11,8 @@ def get_level():
         try:
             lev = int(input("Level: "))
             if 1 <= lev <= 3:
-                generate_integer(lev)
-                return
+                ans = generate_integer(lev)
+                return ans
             else:
                 pass
 
@@ -20,13 +21,84 @@ def get_level():
 
 
 def generate_integer(level):
+    score = 0
     match level:
         case 1:
             for i in range(10):
               x = random.randint(0,9)
               y = random.randint(0,9)
-              
+              i = 0
+              while i < 3:
+                  try:
+                      sum = int(input(f"{x} + {y} = "))
+                      if sum == x + y:
+                          score += 1
+                          break
+                      elif sum != x + y and i < 2:
+                          print("EEE")
+                          i += 1
+                      elif i == 2:
+                          i = 0
+                          print(f"{x} + {y} = {x+y}")
+                          break
+                  except ValueError:
+                      if i < 2:
+                          i += 1
+                      elif i == 2:
+                          print(f"{x} + {y} = {x+y}")
+                          break
 
+        case 2:
+            for i in range(10):
+              x = random.randint(10,99)
+              y = random.randint(10,99)
+              i = 0
+              while i < 3:
+                  try:
+                      sum = int(input(f"{x} + {y} = "))
+                      if sum == x + y:
+                          score += 1
+                          break
+                      elif sum != x + y and i < 2:
+                          print("EEE")
+                          i += 1
+                      elif i == 2:
+                          i = 0
+                          print(f"{x} + {y} = {x+y}")
+                          break
+                  except ValueError:
+                      if i < 2:
+                          i += 1
+                      elif i == 2:
+                          print(f"{x} + {y} = {x+y}")
+                          break
+
+        case 3:
+            for i in range(10):
+              x = random.randint(100,999)
+              y = random.randint(100,999)
+              i = 0
+              while i < 3:
+                  try:
+                      sum = int(input(f"{x} + {y} = "))
+                      if sum == x + y:
+                          score += 1
+                          break
+                      elif sum != x + y and i < 2:
+                          print("EEE")
+                          i += 1
+                      elif i == 2:
+                          i = 0
+                          print(f"{x} + {y} = {x+y}")
+                          break
+                  except ValueError:
+                      if i < 2:
+                          i += 1
+                      elif i == 2:
+                          print(f"{x} + {y} = {x+y}")
+                          break
+
+    return score
 
 
 if __name__ == "__main__":
