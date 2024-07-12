95aebdeb62866d12176913bc6dd3ce4f8cf9bef5
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-12 06:05:48 +0000

    Fri, Jul 12, 2024, 11:35 AM +05:30

--- projects/week4/adieu.py
+++ projects/week4/adieu.py
@@ -2,25 +2,12 @@ def main():
   l = ["Adieu,", "adieu,", "to"]
   try:
     while True:
-      if len(l) < 4:
-        name = input("Name: ")
-        l.append(name+",")
-      elif len(l) >= 4:
         name = input("Name: ")
         l.append(name+",")
 
   except EOFError:
     pass
-  if len(l) > 4:
-    b = ""
-    a = ""
-    b = b + l[-1].rstrip(",")
-    a = a + l[-2].rstrip(",")
-    l.pop()
-    l.pop()
-    l.append(a)
-    l.append("and")
-    l.append(b)
+
   return l
 
 q = main()
