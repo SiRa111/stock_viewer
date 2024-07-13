96a635eee2c0ccde58288aece609e8aa8c65eab5
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-13 10:53:07 +0000

    Sat, Jul 13, 2024, 4:23 PM +05:30

--- projects/week4/bitcoin.py
+++ projects/week4/bitcoin.py
@@ -2,9 +2,15 @@ import json
 import requests
 import sys
 
+_ = request.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
 def main():
-  try:
-    if len(sys.argv) = 
+  if len(sys.argv) == 1:
+    print("Missing command-line argument")
+    sys.exit()
+  elif len(sys.argv) == 2:
+      try:
+         bc = float(sys.argv)
 
 
 if __name__ == "__main__":
