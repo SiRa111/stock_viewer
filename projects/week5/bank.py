edd3201cd37e03ae6fb24085300f13c9f132d3fc
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-15 13:18:53 +0000

    Mon, Jul 15, 2024, 6:48 PM +05:30

--- projects/week5/bank.py
+++ projects/week5/bank.py
@@ -1,16 +1,21 @@
+g = input("Greeting : ")
+g1 = g.strip().lower()
+
+
 def main():
-    if g1.startswith("hello"):
-        return "$0"
-    elif g1.startswith("h"):
-        return "$20"
-    else:
-        return "$100"
+    print(value(g1))
+
 
 def value(greeting):
-    g = input("Greeting : ")
-    g1 = g.strip().lower()
+  if greeting.startswith("hello"):
+      return "0"
+  elif greeting.startswith("h"):
+      return "20"
+  else:
+      return "100"
 
 
-a = main()
-print(a)
+
+if __name__ == "__main__":
+    main()
 
