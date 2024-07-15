09408b18dc1122e05651dbd402737da0b8228c34
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-15 07:37:08 +0000

    Mon, Jul 15, 2024, 1:07 PM +05:30

--- lec_5/test_convert.py
+++ lec_5/test_convert.py
@@ -5,3 +5,9 @@ from convert import convert
 def test_converion():
   assert convert(1) == 149597870700
   assert convert(50) == 7479893535000
+
+
+def test_error():
+  with pytest.raises(TypeError):
+    convert("1") #an error will be raised if we give a string
+    "a type error"
