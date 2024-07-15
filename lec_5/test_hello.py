d3930fec14f35574ffa3de8294435d9b4fc7a929
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-15 06:48:02 +0000

    Mon, Jul 15, 2024, 12:18 PM +05:30

--- lec_5/test_hello.py
+++ lec_5/test_hello.py
@@ -1,6 +1,11 @@
 from hello import hello
 
-def test_hello():
-  assert hello("Simran") == "hello, Simran"
-  assert hello() == "hello
+def test_default():
+  assert hello() == "hello, world"
 
+def test_argument():
+    assert hello("Simran") == "hello, Simran"
+
+def test_list():#tests multiple values 
+   for name in ["Hermionie", "Harry", "Ron"]:
+      assert hello(name) == f"hello, {name}"
