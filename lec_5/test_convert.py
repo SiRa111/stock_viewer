1f81bf2abc54bd6478ceb893c0a85dc151bbc080
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-15 07:43:00 +0000

    Mon, Jul 15, 2024, 1:13 PM +05:30

--- lec_5/test_convert.py
+++ lec_5/test_convert.py
@@ -11,3 +11,11 @@ def test_error():
   with pytest.raises(TypeError):
     convert("1") #an error will be raised if we give a string
     "a type error"
+
+
+def test_float_conversion():
+  'assert convert(0.001) == 149597870.691'
+  #this gives error due to the floating pt
+
+  assert convert(0.001) == pytest.approx(149597870.691)
+  #allows some float tolerance
