312ef7956bfb5e307bdf8b8b3d49f71314120770
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-15 12:55:47 +0000

    Mon, Jul 15, 2024, 6:25 PM +05:30

--- projects/week5/test_twttr.py
+++ projects/week5/test_twttr.py
@@ -1,22 +1,16 @@
 from twttr import shorten
-import sys
 
 
-def main():
-  try:
-    def test_lower():
-      assert shorten("short word") == "shrt wrd"
+def test_lower():
+  assert shorten("short word") == "shrt wrd"
 
-    def test_upper():
-      assert shorten("UPPERCASE HUH") == "PPRCS HH"
+def test_upper():
+  assert shorten("UPPERCASE HUH") == "PPRCS HH"
 
-    def test_num():
-      assert shorten("1234") == "1234"
+def test_num():
+  assert shorten("1234") == "1234"
 
-    def test_punc():
-      assert shorten("hi!we:that.") == "h!w:tht"
+def test_punc():
+  assert shorten("hi!we:that.") == "h!w:tht"
 
-    sys.exit(1)
 
-if __name__ == "__main__":
-  main()
