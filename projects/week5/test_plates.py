7338ae0e9c264690bf5025ae68a3c84a3f054258
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-16 09:04:53 +0000

    Tue, Jul 16, 2024, 2:34 PM +05:30

--- projects/week5/test_plates.py
+++ projects/week5/test_plates.py
@@ -3,4 +3,19 @@ from plates import is_valid
 def test_alpha():
   assert is_valid("q") == "Invalid"
   assert is_valid("QWERTYU") == "Invalid"
+
+
+def test_first_two():
+  assert is_valid("54gh") == "Invalid"
+  assert is_valid("jk") == "Valid)
+  assert is_valid("jkl90") == "Valid"
+
+
+def test_only_alnum():
+  assert is_valid("gh_ij") == "Invalid"
+  assert is_valid("whoa:90") == "Invalid"
+  assert is_valid("welp./87") == "Invalid"
+  assert is_valid("ghji980") == "Valid"
+
+def def first_non_zero():
   assert is_valid("")
