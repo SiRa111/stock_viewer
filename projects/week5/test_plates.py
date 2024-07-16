8cf535c43d66abfa430414920865a2e69ae86dff
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-16 09:15:51 +0000

    Tue, Jul 16, 2024, 2:45 PM +05:30

--- projects/week5/test_plates.py
+++ projects/week5/test_plates.py
@@ -1,26 +1,29 @@
 from plates import is_valid
 
+
 def test_alpha():
-  assert is_valid("q") == False
-  assert is_valid("QWERTYU") == False
+    assert is_valid("q") == False
+    assert is_valid("QWERTYU") == False
 
 
 def test_first_two():
-  assert is_valid("54gh") == False
-  assert is_valid("jk") == True
-  assert is_valid("jkl90") == True
+    assert is_valid("54gh") == False
+    assert is_valid("jk") == True
+    assert is_valid("jkl90") == True
 
 
 def test_only_alnum():
-  assert is_valid("gh_ij") == False
-  assert is_valid("whoa:90") == False
-  assert is_valid("welp./87") == False
-  assert is_valid("ghji980") == True
+    assert is_valid("gh_ij") == False
+    assert is_valid("whoa:90") == False
+    assert is_valid("welp./87") == False
+    assert is_valid("ghj980") == True
 
-def first_non_zero():
-  assert is_valid("gh009") == False
-  assert is_valid("gh909") == True
 
-def end_num():
-  assert is_valid("gh09ji") == False
-  assert is_valid("ghji90") == True
+def test_first_non_zero():
+    assert is_valid("gh009") == False
+    assert is_valid("gh909") == True
+
+
+def test_end_num():
+    assert is_valid("gh09ji") == False
+    assert is_valid("ghji90") == True
