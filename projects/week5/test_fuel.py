4baa7d010e98b24e0ca41abf0d6c3f138bbd2312
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-16 11:59:07 +0000

    Tue, Jul 16, 2024, 5:29 PM +05:30

--- projects/week5/test_fuel.py
+++ projects/week5/test_fuel.py
@@ -1,4 +1,16 @@
-import fuel
+from fuel import convert, gauge
 
-def valid_fraction():
-  assert 
+
+def test_valid_fraction():
+  assert convert("9/9") == 1
+  assert convert("8/0") == ZeroDivisionError
+  assert convert("8/2") == ValueError
+  assert convert("8/9") == 89
+
+
+def test_outputs():
+  assert gauge(10) == "10%"
+  assert gauge(78) == "78%"
+  assert gauge(1) == "E"
+  assert gauge(99) == "F"
+  assert gauge(100) == "F"
