4d901d45af40dd4f4971733c6f81b657c3a5b6cf
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-16 09:19:08 +0000

    Tue, Jul 16, 2024, 2:49 PM +05:30

--- projects/week5/test_plates.py
+++ projects/week5/test_plates.py
@@ -4,13 +4,14 @@ from plates import is_valid
 def test_alpha():
     assert is_valid("q") == False
     assert is_valid("QWERTYU") == False
+    assert is_valid("QWerTy") == False
 
 
 def test_first_two():
     assert is_valid("54gh") == False
     assert is_valid("jk") == True
     assert is_valid("jkl90") == True
-    assert is_valid("34567")
+    assert is_valid("34567") == False
 
 
 def test_only_alnum():
