4989fa363c3d9651fe5de4c43dabcdd09ffb92c5
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-17 07:04:09 +0000

    Wed, Jul 17, 2024, 12:34 PM +05:30

--- projects/week5/test_fuel.py
+++ projects/week5/test_fuel.py
@@ -4,7 +4,7 @@ from fuel import convert, gauge
 def test_valid_fraction():
   assert convert("9/9") == 1
   assert convert("8/0") == <class 'ZeroDivisionError'>
-  assert convert("8/2") == < class 'ValueError'>
+  assert convert("8/2") == <class 'ValueError'>
   assert convert("8/9") == 89
 
 
