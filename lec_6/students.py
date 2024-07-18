969f40669c4f14bfa24597224209a7ed7fe65b3d
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-18 09:57:48 +0000

    Thu, Jul 18, 2024, 3:27 PM +05:30

--- lec_6/students.py
+++ lec_6/students.py
@@ -3,4 +3,9 @@ students = []
 with open("students.csv") as file:
   for line in file:
     name, house = line.rstrip().split(",")
-    
+    student = {"name":name, "house":house}
+    students.append(student)
+
+
+for student in students:
+  print(f"{student['name']} is in {student['house']}")
