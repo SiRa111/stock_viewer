8458c0309ae751eecdf94a70e69eb9e7534a2d91
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-18 10:02:06 +0000

    Thu, Jul 18, 2024, 3:32 PM +05:30

--- lec_6/students.py
+++ lec_6/students.py
@@ -8,7 +8,7 @@ with open("students.csv") as file:
 
 
 def get_name(student):
-  return student
+  return student["name"]
 
 for student in sorted(students, key=get_name):
   print(f"{student['name']} is in {student['house']}")
