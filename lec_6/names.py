08b721d26fe4cb6f59d64b4ed45aad260a95363a
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-18 08:47:37 +0000

    Thu, Jul 18, 2024, 2:17 PM +05:30

---
+++ lec_6/names.py
@@ -0,0 +1,11 @@
+names = []
+#a list to gather the txt data
+
+
+with open("names.txt") as file: #default is r mode
+  for line in file:
+    names.append(line.rstrip()) #we dont want newlines
+
+
+for name in sorted(names): #alphetically sorted list
+  print(f"hello, {name}")
