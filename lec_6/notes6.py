8534fa9be7454d61842e8cbcb069b5268a8f3748
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-18 06:54:23 +0000

    Thu, Jul 18, 2024, 12:24 PM +05:30

--- lec_6/notes6.py
+++ lec_6/notes6.py
@@ -3,3 +3,22 @@ list
 tuple
 different strings
 integers
+
+
+0 code to enter data in a file:
+
+
+name = input("Whats your name? ")
+
+'''
+file = open("names.txt", "a") #append mode
+file.write(f"{name}\n")
+file.close()
+'''
+
+"""
+METHOD WITH NO CLOSIGN TAG
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+"""
