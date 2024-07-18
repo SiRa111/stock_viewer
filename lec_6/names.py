3ec87046914af21ff01dc388a22537ac956426f8
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-18 06:53:27 +0000

    Thu, Jul 18, 2024, 12:23 PM +05:30

--- lec_6/names.py
+++ lec_6/names.py
@@ -1,5 +1,14 @@
 name = input("Whats your name? ")
 
+'''
 file = open("names.txt", "a") #append mode
 file.write(f"{name}\n")
 file.close()
+'''
+
+"""
+METHOD WITH NO CLOSIGN TAG
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+"""
