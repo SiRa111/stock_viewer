90f1fc35276de3a9a6f526c63529d15fbb248120
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-19 08:49:28 +0000

    Fri, Jul 19, 2024, 2:19 PM +05:30

--- lec_6/students.py
+++ lec_6/students.py
@@ -6,9 +6,16 @@ with open("students.csv") as file:
     student = {"name":name, "house":house}
     students.append(student)
 
+
+'''
 #SORTING ON THE BASIS OF HOUSE
 def get_house(student):
   return student["house"]
 
 for student in sorted(students, key=get_house, reverse = True):
+  print(f"{student['name']} is in {student['house']}")
+'''
+
+#___________USING LABDA FUNCTION___________________
+for student in sorted(students, key=lambda student: student["name"]):
   print(f"{student['name']} is in {student['house']}")
