433b854d2a913c877f8f1c4fb80451ff87795f42
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-19 09:15:39 +0000

    Fri, Jul 19, 2024, 2:45 PM +05:30

--- lec_6/notes6.py
+++ lec_6/notes6.py
@@ -26,7 +26,25 @@ with open("names.txt", "a") as file:
 """
 
 
+'''
+#SORTING ON THE BASIS OF HOUSE
+def get_house(student):
+  return student["house"]
+
+for student in sorted(students, key=get_house, reverse = True):
+  print(f"{student['name']} is in {student['house']}")
+'''
+
+#___________USING LABDA FUNCTION___________________
+for student in sorted(students, key=lambda student: student["name"]):
+  print(f"{student['name']} is in {student['house']}")
+
+
 LAMBDA FXN
 it only needs to be called once
 so we dont name it
 anonymous
+
+
+CSV MODULE
+
