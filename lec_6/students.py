3269f5e4510683a372524709fcc19f03ef4115b2
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-20 07:49:15 +0000

    Sat, Jul 20, 2024, 1:19 PM +05:30

--- lec_6/students.py
+++ lec_6/students.py
@@ -6,4 +6,5 @@ home = input("Where is your home? ")
 with open("students.csv", "a") as file:
   writer = csv.DictWriter(file, fieldnames=["name","home"])
   #fieldnames are the arguments to dict writer
+  #DictReader returns one dict at a time
   writer.writerow({"name":name, "home":home})
