5e329bd4ce037f499c9b4e1229b08173ea168cd9
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-21 12:30:53 +0000

    Sun, Jul 21, 2024, 6:00 PM +05:30

---
+++ trying.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+try:
+    img = Image.open("C:\Users\Administrator\Downloads\costume1.gif")
+    # Do something with img
+except IOError as e:
+    print("Error opening image:", e)
