e7fd0f052d4b6602719f7a2877f1d218c3d6b702
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-22 12:21:40 +0000

    Mon, Jul 22, 2024, 5:51 PM +05:30

--- projects/week6/lines.py
+++ projects/week6/lines.py
@@ -21,7 +21,7 @@ def count(filename):
         with open(f"{filename}", "r") as _:
             om = _.readlines()
             for whoa in om:
-                whoa = whoa.strip()
+                whoa = whoa.lstrip()
                 file.write(whoa)
 
 
