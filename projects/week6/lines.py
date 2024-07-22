4d30b95e9d64e78fe8d48630a1c7a7bf0eddbac4
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-22 07:14:10 +0000

    Mon, Jul 22, 2024, 12:44 PM +05:30

--- projects/week6/lines.py
+++ projects/week6/lines.py
@@ -2,8 +2,22 @@ import sys
 
 
 def main():
+    if len(sys.argv) == 1:
+        print("Too few commnad-line arguments")
+        sys.exit()
+    elif len(sys.argv) >= 3:
+        print("Too many command-line arguments")
+        sys.exit()
+    else:
+        if '.py' in sys.argv[1]:
+            count(sys.argv[1])
+        else:
+            print("Not a Python file")
+            sys.exit()
 
 
+def count(filename):
+    
 
 
 if  __name__ == "__main__":
