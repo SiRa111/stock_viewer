fa4309d6daf118afff0df9653b206d839ff3bb7c
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-24 08:39:52 +0000

    Wed, Jul 24, 2024, 2:09 PM +05:30

--- projects/week6/pizza.py
+++ projects/week6/pizza.py
@@ -1,9 +1,30 @@
+import sys
+import csv
+
 def main():
-    try:
-        if len(sys.argv) == 2
-        elif len(sys.argv) < 2
-        elif len(sys.argv) > 2
-    except FileNotFoundError
+    if len(sys.argv) == 2:
+        if sys.argv[1].endswith(".csv"):
+            try:
+                pizza(sys.argv[1])
+            except FileNotFoundError:
+                print("File not found")
+                sys.exit(1)
+        else:
+            print("Not a CSV file")
+            sys.exit(1)
+
+    elif len(sys.argv) < 2:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+
+
+def pizza(csvfile):
+    with open(f"{csvfile}", "r") as file:
+        reader = DictReader(file)
+        reader = readlines
 
 
 if __name__ == "__main__":
