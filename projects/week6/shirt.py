bddc3e1c2276cddf07f46e5fa94e65e2a4e8acff
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-25 10:15:04 +0000

    Thu, Jul 25, 2024, 3:45 PM +05:30

--- projects/week6/shirt.py
+++ projects/week6/shirt.py
@@ -2,7 +2,18 @@ import sys
 from PIL import Image
 
 def main():
-    
+    if len(sys.argv) < 3:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 3:
+        print("Too many command-line arguments")
+        sys.exit(1)
+    else:
+        sys.argv[1] = input_img
+        sys.argv[2] = output_img
+        if input_img and output_img.
+
+
 
 
 if __name__ == "__main__":
