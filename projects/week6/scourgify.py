52917f18f9b3b92e553e0e6e0df2f789bcfc1242
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-07-25 07:29:39 +0000

    Thu, Jul 25, 2024, 12:59 PM +05:30

--- projects/week6/scourgify.py
+++ projects/week6/scourgify.py
@@ -3,9 +3,22 @@ import sys
 
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Too few command-line arguments")
         sys.exit(1)
+    elif len(sys.argv) > 3:
+        print("Too many command-line arguments")
+        sys.exit()
+    else:
+        try:
+            clean(sys.argv[1], sys.argv[2])
+        except FileNotFoundError:
+            print("File not found")
+            sys.exit(1)
+
+
+def clean(nsorted, sorted):
+    ...
 
 
 
