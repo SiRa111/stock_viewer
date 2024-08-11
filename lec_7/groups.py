b01f999c0e5a49c0c75a3b1c2920296669dd2ae6
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-11 08:44:03 +0000

    Sun, Aug 11, 2024, 2:14 PM +05:30

--- lec_7/groups.py
+++ lec_7/groups.py
@@ -3,7 +3,7 @@ import re
 locations = {"+1": "United States and Canada", "+62": "Indonesia", "+505": "Nicaragua"}
 
 def main():
-    pattern = r"\+\d{1,3} \d{3}-\d{3}-\d{4}"
+    pattern = r"(?P<country_code>\+\d{1,3}) \d{3}-\d{3}-\d{4}"
     #\+ : we literally want +
     #\d : we want decimal digits ie 0 to 9
     #they can be 1 or 3 digits
