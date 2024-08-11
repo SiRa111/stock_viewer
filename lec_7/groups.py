3005184b3c8fb3f4d1a428fadd1a73ff54ae0a37
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-11 08:21:36 +0000

    Sun, Aug 11, 2024, 1:51 PM +05:30

--- lec_7/groups.py
+++ lec_7/groups.py
@@ -3,5 +3,15 @@ import re
 locations = {"+1": "United States and Canada", "+62": "Indonesia", "+505": "Nicaragua"}
 
 def main():
-    pattern = r"\"
+    pattern = r"\+\d{1,3} \d{3}-\d{3}-\d{4}"
+    #\+ : we literally want +
+    #\d : 
+
+    number = input("Number: ")
+
+    match = re.search(pattern, number)
+    if match:
+        print("Valid")
+    else:
+        print("Invalid")
 
