59692d75bfc909ad0de7ae8d7feb318ee074d558
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-11 07:24:43 +0000

    Sun, Aug 11, 2024, 12:54 PM +05:30

--- lec_7/twitter.py
+++ lec_7/twitter.py
@@ -2,4 +2,6 @@ import re
 
 url = input("URL: ").strip()
 
- if matches := re.search(r"https?//www\.twitter\.com/.+$", url, re.IGNORECASE)
+if matches := re.search(r"^https?//(?:www\.)twitter\.com/(.+)$", url, re.IGNORECASE:
+                        print(f"Username:", matches.group(1))
+
