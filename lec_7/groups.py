c1040236eac2f3770dc97aa1af527097946d2f85
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-11 08:48:25 +0000

    Sun, Aug 11, 2024, 2:18 PM +05:30

--- lec_7/groups.py
+++ lec_7/groups.py
@@ -7,13 +7,15 @@ def main():
     #\+ : we literally want +
     #\d : we want decimal digits ie 0 to 9
     #they can be 1 or 3 digits
-    
+    #it is named country code 
+
 
 
     number = input("Number: ")
 
     match = re.search(pattern, number)
     if match:
+        country_code = match.group("country_code")
         print("Valid")
     else:
         print("Invalid")
