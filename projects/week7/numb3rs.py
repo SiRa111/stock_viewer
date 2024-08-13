4957cb89945b214aaa068e9e9348d2dd44058ef0
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-13 12:40:34 +0000

    Tue, Aug 13, 2024, 6:10 PM +05:30

--- projects/week7/numb3rs.py
+++ projects/week7/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    matches = re.search(r"", ip)
+    matches = re.search(r"[0-255]\.[0-255]\.[0-255]\.[0-255]\.", ip)
     if matches == True:
         return True
     else:
