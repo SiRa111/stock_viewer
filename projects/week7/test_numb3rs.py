debc90bb68172d32bbf4a65795aa417544adedae
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-13 14:41:35 +0000

    Tue, Aug 13, 2024, 8:11 PM +05:30

--- projects/week7/test_numb3rs.py
+++ projects/week7/test_numb3rs.py
@@ -11,6 +11,7 @@ def test_valid_nums():
   assert validate("276.0.0.1") == False
   assert validate("3000.5.6.7") == False
   assert validate("567.890.1000.5783945") == False
+  assert validate("1.2.3.4.5") == False
 
 def test_no_extra_chars():
   assert validate("0.0.0.0.") == False
