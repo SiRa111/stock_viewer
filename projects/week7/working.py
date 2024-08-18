405189b7e5d40d4d9ec1327bc08bc53665975ff6
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-18 08:06:54 +0000

    Sun, Aug 18, 2024, 1:36 PM +05:30

--- projects/week7/working.py
+++ projects/week7/working.py
@@ -9,9 +9,9 @@ def main():
 def convert(s):
     if match := re.search('([1-12]):([0-5]/d)?\s(am|AM|pm|PM)\sto\s([1-12]):([0-5]/d)?\s(?:am|AM|pm|PM)'):
         if match.group(3) == ('am'|'AM'):
-            print(f"{match.group(1)}:{match.group(2) to {match.group(4)+12}:{match.group(5)}}")
+            print(f"{match.group(1)}:{match.group(2)} to {match.group(4)+12}:{match.group(5)}")
         else:
-            print(f"{match.group(1)+12}:{match.group(2) to {match.group(4)}:{match.group(5)}}")
+            print(f"{match.group(1)+12}:{match.group(2)} to {match.group(4)}:{match.group(5)}")
     else:
         raise ValueError("ValueError")
 
