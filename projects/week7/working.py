5a949a5cb610f1b5d8560ed4e8bd963ffa842d9a
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-18 11:08:25 +0000

    Sun, Aug 18, 2024, 4:38 PM +05:30

--- projects/week7/working.py
+++ projects/week7/working.py
@@ -9,9 +9,9 @@ def main():
 def convert(s):
     if match := re.search(r'([1-12]):?([0-5]\d)?\s(am|AM|pm|PM)\sto\s([1-12]):?([0-5]\d)?\s(?:am|AM|pm|PM)', s):
         if match.group(3).lower() == 'am' :
-            print(f"{match.group(1)}:{match.group(2)} to {int(match.group(4))+12}:{match.group(5)}")
+            print(f"{match.group(1)}:{match.group(2)} to {int(match.group(4))}:{match.group(5)}")
         else:
-            print(f"{int(match.group(1))+12}:{match.group(2)} to {match.group(4)}:{match.group(5)}")
+            print(f"{int(match.group(1))}:{match.group(2)} to {match.group(4)}:{match.group(5)}")
     else:
         raise ValueError("ValueError")
 
