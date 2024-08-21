98013ecd4f0397a307277d4556bb9b35f2a1a2cc
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-21 05:06:00 +0000

    Wed, Aug 21, 2024, 10:36 AM +05:30

--- projects/week7/working.py
+++ projects/week7/working.py
@@ -9,10 +9,16 @@ def main():
 def convert(s):
     if match := re.search(r'([1-12]):?([0-5]\d)?\s(am|AM|pm|PM)\sto\s([1-12]):?([0-5]\d)?\s(am|AM|pm|PM)', s):
         if match.group(3) == ('am' or 'AM'):
-            if match.group(2) == None:
-                min1 == "00"
-            else:
-                pass
+            if (match.group(2) and match.group(5)) == None:
+                min1 = "00"
+                min2 = "00"
+            elif match.group(2) == None:
+                min1 = "00"
+                match.group(5) = min2
+            elif match.group(5) == None:
+                match.group(2) = min1
+                min2 = "00"
+
 
 
         else:
