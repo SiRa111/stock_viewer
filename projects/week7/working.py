383a28ba0ef820dd833133240cbe0f1831bd3528
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-21 04:36:51 +0000

    Wed, Aug 21, 2024, 10:06 AM +05:30

--- projects/week7/working.py
+++ projects/week7/working.py
@@ -9,7 +9,8 @@ def main():
 def convert(s):
     if match := re.search(r'([1-12]):?([0-5]\d)?\s(am|AM|pm|PM)\sto\s([1-12]):?([0-5]\d)?\s(am|AM|pm|PM)', s):
         if match.group(3) == ('am' or 'AM'):
-            ...
+            if match.group(4) == 12:
+                return f"{match.group(1)}:{match.group(2)}
         else:
             ...
 
