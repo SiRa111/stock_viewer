4c5bbe601b105c7cee46c040cdb38d10c0125876
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-24 15:29:17 +0000

    Sat, Aug 24, 2024, 8:59 PM +05:30

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+import nparray from numpy
 
-num = input("ENTER YOUR BODY COUNT")
+num = input("ENTER YOUR BODY COUNT: ")
 
 umm = []
 
-for _ in range(len(num), [:-1]):
+for _ in range(len(num),0, -1):
     umm.append(_)
 
 final = umm.nparray()
