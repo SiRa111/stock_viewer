91f04c99bce28e103793449321ad2166404ac2f2
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-24 16:03:50 +0000

    Sat, Aug 24, 2024, 9:33 PM +05:30

--- tits.py
+++ tits.py
@@ -4,30 +4,32 @@ def main():
       print(i)
 
 
-def issum(n):
+def issum(nigah):
+  n = str(nigah)
   uemp = []
   for _ in range(len(n)):
     uemp.append(n[_])
 
   s = 0
 
-  for _ in len(uemp):
-    s = s + _
+  for _ in range(len(uemp)):
+    s = s + uemp[_]
 
   if s == n:
     return True
   else:
     return False
 
-def isprod(n):
+def isprod(nigah):
+  n = str(nigah)
   uimp = []
   for _ in range(len(n)):
     uimp.append(n[_])
 
   p = 1
 
-  for _ in len(uimp):
-    p = p * _
+  for _ in range(len(uimp)):
+    p = p * uimp[_]
 
   if p == n:
     return True
