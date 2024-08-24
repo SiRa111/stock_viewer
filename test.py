07d685484ec02056d427d73404a90471719d7798
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-24 15:50:30 +0000

    Sat, Aug 24, 2024, 9:20 PM +05:30

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 
 def main():
+  for i in range(100):
+    if issum(i) == True and isprod(i) == True:
+      print(i)
 
 
 def issum(n):
@@ -10,7 +13,30 @@ def issum(n):
   s = 0
 
   for _ in len(uemp):
-    
+    s = s + _
+
+  if s == n:
+    return True
+  else:
+    return False
+
+def isprod(n):
+  uimp = []
+  for _ in len(n):
+    uimp.append(_)
+
+  p = 1
+
+  for _ in len(uimp):
+    p = p * _
+
+  if p == n:
+    return True
+  else:
+    return False
+
+main()
+
 
 
 
