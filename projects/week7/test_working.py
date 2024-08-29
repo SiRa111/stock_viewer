c7bbb99ad38fa39d6deb14c6128a67dbd00c0df6
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-29 13:38:49 +0000

    Thu, Aug 29, 2024, 7:08 PM +05:30

--- projects/week7/test_working.py
+++ projects/week7/test_working.py
@@ -1,7 +1,10 @@
 from working import convert
 
 def test_format():
-    assert convert("9:60 AM to 5:60 PM") == False
-    assert convert("9 AM - 5 PM ") == False
-    assert convert("9:00 AM - 5:00 PM") == False
+    with assertRaises(ValueError):
+        convert("9:60 AM to 5:60 PM") == False
+    with assertRaises(ValueError):
+        convert("9 AM - 5 PM ") == False
+    with assertRaises(ValueError):
+        convert("9:00 AM - 5:00 PM") == False
 
