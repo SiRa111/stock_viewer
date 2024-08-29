cacde40b49e3826ef7ac55642cf7ddaccacf33fc
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-29 15:00:36 +0000

    Thu, Aug 29, 2024, 8:30 PM +05:30

--- projects/week7/test_working.py
+++ projects/week7/test_working.py
@@ -1,27 +1,27 @@
-from working import convert  # Replace 'working' with the actual name of your script
+from working import convert  
 
 def test_format():
-    # Test 1: Invalid minutes
+
     try:
         convert("9:60 AM to 5:60 PM")
     except ValueError:
-        pass  # Test passed
+        pass
     else:
         assert False, "Test failed: ValueError not raised for '9:60 AM to 5:60 PM'"
 
-    # Test 2: Incorrect time separator
+
     try:
         convert("9 AM - 5 PM")
     except ValueError:
-        pass  # Test passed
+        pass
     else:
         assert False, "Test failed: ValueError not raised for '9 AM - 5 PM'"
 
-    # Test 3: Incorrect format
+    orrect format
     try:
         convert("9:00 AM - 5:00 PM")
     except ValueError:
-        pass  # Test passed
+        pass
     else:
         assert False, "Test failed: ValueError not raised for '9:00 AM - 5:00 PM'"
 
