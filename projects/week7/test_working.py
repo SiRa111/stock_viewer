057adf9d64b4d6caf6e3fe47d3a8dcac406e1252
SiRa111 <146064214+SiRa111@users.noreply.github.com>
2024-08-29 13:42:37 +0000

    Thu, Aug 29, 2024, 7:12 PM +05:30

--- projects/week7/test_working.py
+++ projects/week7/test_working.py
@@ -2,9 +2,9 @@ from working import convert
 
 def test_format(v):
     with v.assertRaises(ValueError):
-        convert("9:60 AM to 5:60 PM") 
+        convert("9:60 AM to 5:60 PM")
     with v.assertRaises(ValueError):
-        convert("9 AM - 5 PM ") 
+        convert("9 AM - 5 PM ")
     with v.assertRaises(ValueError):
         convert("9:00 AM - 5:00 PM") 
 
