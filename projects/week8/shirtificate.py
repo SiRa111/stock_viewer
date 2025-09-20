6cbd24443e509ad13cb4eb71a424a69f1a4553de
Simran <146064214+SiRa111@users.noreply.github.com>
2025-09-20 07:50:21 +0000

    Sat, Sep 20, 2025, 1:20 PM +05:30

--- projects/week8/shirtificate.py
+++ projects/week8/shirtificate.py
@@ -10,11 +10,11 @@ pdf.set_font("Arial", "B", 24)
 pdf.set_text_color(0, 0, 0)
 pdf.cell(0, 30, "CS50 Shirtificate", align="C", ln=1)
 
-pdf.image("shirtificate.png", x=55, w=100)
+pdf.image("shirtificate.png", x=55, y=60, w=100)
 
 pdf.set_font("Arial", "B", 20)
 pdf.set_text_color(255, 255, 255)
-pdf.set_xy(0, 120)
+pdf.set_xy(0, 90)
 pdf.cell(0, 10, name, align="C")
 
 pdf.output("shirtificate.pdf")
